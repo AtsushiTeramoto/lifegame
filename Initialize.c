9b2780a6bc45b0fb49e4a0d22a7e6bdacb845418
Atsushi Teramoto <k5013541@kadai.jp>
2016-08-14 06:44:40 +0900

    メモリ確保に失敗した場合はabort

--- Initialize.c
+++ Initialize.c
@@ -2,12 +2,20 @@
 #include"Structures.h"
 #include"Initialize.h"
 
+static void* malloc_and_check(const size_t size)
+{
+    void *p = malloc(size);
+    if (p == NULL) {
+        abort();
+    }
+    return p;
+}
 bool ** Alloc_Cells(int max_x, int max_y)
 {
     bool **cells;
-    cells = (bool **)malloc(sizeof(bool*)*max_x);
+    cells = (bool **)malloc_and_check(sizeof(bool*)*max_x);
     for (int i = 0; i < max_x; i++) {
-        cells[i] = (bool *)malloc(sizeof(bool)*max_y);
+        cells[i] = (bool *)malloc_and_check(sizeof(bool)*max_y);
     }
     return cells;
 }
@@ -26,7 +34,7 @@ static void Preset_Cells(struct lifegame_field *field)
 struct lifegame_field* Initialize(int max_x,int max_y)
 {
     struct lifegame_field *field;
-    field = malloc(sizeof(struct lifegame_field));
+    field = malloc_and_check(sizeof(struct lifegame_field));
     field->max_x = max_x;
     field->max_y = max_y;
     field->cells = Alloc_Cells(field->max_x,field->max_y);
